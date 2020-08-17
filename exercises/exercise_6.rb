55eb1afd5554f0bffae519fbaebb5329fa8be808
NatalieSokolova <nataliesklv@gmail.com>
2020-08-17 21:26:00 +0000

    creates more employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,13 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+
+class Store
+  has_many :employees
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Natalie", last_name: "Sokolova", hourly_rate: 85)
+@store2.employees.create(first_name: "Nick", last_name: "Maniutin", hourly_rate: 75)
+@store2.employees.create(first_name: "Harley", last_name: "Quinn", hourly_rate: 50)
+
